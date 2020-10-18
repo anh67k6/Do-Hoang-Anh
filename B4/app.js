d77f39af16385ac00b7fdf2bd2987c58ba935940
anh56k5 <anh67k6>
2020-10-18 21:25:51 +0700

    initial commit

---
+++ B4/app.js
@@ -0,0 +1,96 @@
+// ;const s = 'Bo em hut rat nhieu thuoc. Me em nuoc mat le nhoa'
+
+//Thuat toan => In
+//Điều kiện đầu 
+// const countData= {
+// };
+// for ( let i = 0; i < s.length; i ++){
+//     // console.log(s[i]);
+//     //Lắt nhắt + dài
+//     let c = s[i].toLocaleLowerCase();
+
+//     if(countData[c]){
+//         countData[c]++}
+
+//     else {
+//         countData[c] = 1;
+//     };
+// };
+// console.log(countData);
+// const count = 
+
+//Structure 
+
+//1 =>> array
+//for => trùng => tăng 
+// [
+//     {
+//         value : 'b',
+//         count : 1,
+//     },
+//     {
+//         value : 'o',
+//         count : 4,
+//     },
+// ]
+
+
+
+//2 =>> object
+
+
+// {
+//     b : 1,
+//     o : 4,
+// }
+
+
+// const movies = [
+//     {
+//         title : 'The dark knight',
+//         rate : 8.5,
+//         genre : 'Action',
+//     },
+//     {
+//         title : 'Me before you',
+//         rate : 8,
+//         genre : 'Action',
+//     },
+//     {
+//         title : 'Attack on Titans',
+//         rate : 8.4 ,
+//         genre : 'anime',
+//     },
+//     {
+//         title : 'Terminator',
+//         rate : 8.2 ,
+//         genre : 'Action',
+//     },
+//     {   
+//         title : 'The diary',
+//         rate : 8 ,
+//         genre : 'Romance',
+//     },
+// ]
+
+// //Structure 
+// //Thể loại =>> danh sách phim
+// /*
+
+// */
+// let moviesByGroup = {
+//     Action  : [],
+//     Romance : [],
+//     Anime : [],
+// };
+
+// for ( let i = 0; i< movies.length ; i ++){
+//     const movie = movies[i];
+//     console.log(movie);
+//     let genre = movie.genre;
+//     console.log(genre);
+//     moviesByGroup[genre].push(movie);
+//     };
+//     // console.log(moviesByGroup)
+
+
---
+++ B4/checkpoint.js
@@ -0,0 +1,4 @@
+let a = Math.random().toFixed(2)
+console.log(a);
+
+let show = myShows[Math.floor(Math.random() * myShows.length)];
